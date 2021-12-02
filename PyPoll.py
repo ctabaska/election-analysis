daf37f613ca4a1de384e407ed6f250792e7b69f5
Christopher Tabaska <christophertabaska@gmail.com>
2021-12-02 14:31:51 -0600

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -4,6 +4,11 @@ import os
 # Create filepath variable
 file_path = os.path.join("Resources", "election_results.csv")
 
+
+total_votes = 0
+
+candidate_votes = {}
+
 # Open election data file
 with open(file_path, 'r') as election_data:
     # Set file reader variable
@@ -13,13 +18,43 @@ with open(file_path, 'r') as election_data:
     header = next(file_reader)
     print(header)
 
-    #for row in file_reader:
-    #    print(row)
+    for row in file_reader:
 
-# go through file and check for unique name,
-    #if unique add to list with vote
+        # Check candidate name and add to dictionary if not in dictionary already
+        candidate_name = row[2]
+        if candidate_name not in candidate_votes.keys():
+            candidate_votes[candidate_name] = {'votes': 1, 'percentage': 0.0}
+        else:
+            candidate_votes[candidate_name]['votes'] += 1
 
-    #if not unique add vote to existing name
+        # Total the votes
+        total_votes += 1
+
+    # Check percentage of votes for each candidate
+    for candidate in candidate_votes:
+        candidate_votes[candidate]['percentage'] = float(candidate_votes[candidate]['votes']) / float(total_votes) * 100
+
+# print candidate data
+for candidate in candidate_votes:
+    print(f"{candidate}: {candidate_votes[candidate]['percentage']:.1f}% ({candidate_votes[candidate]['votes']:,})")
+
+winning_candidate = {'name': '', 'votes': 0, 'percentage': 0}
+
+# Find winner
+for candidate in candidate_votes:
+    if (candidate_votes[candidate]['votes'] > winning_candidate['votes']):
+        winning_candidate['name'] = candidate
+        winning_candidate['votes'] = candidate_votes[candidate]['votes']
+        winning_candidate['percentage'] = candidate_votes[candidate]['percentage']
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate['name']}\n"
+    f"Winning Vote Count: {winning_candidate['votes']:,}\n"
+    f"Winning Percentage: {winning_candidate['percentage']:.1f}%\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
 
 file_path_results = os.path.join("Analysis", "election_anaylsis.txt")
 
@@ -29,4 +64,4 @@ with open(file_path_results, "w") as election_results:
     election_results.write("Arapahoe\nDenver\nJefferson")
     
 
-    print(election_results)
+    #print(election_results)
